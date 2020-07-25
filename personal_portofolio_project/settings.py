3debe125bed1ca12b719c7e5c15dff1d1e8636ef
maikaru13 <anjingbor@ymail.com>
2020-07-25 07:13:30 +0000

    Remove old stuff

--- personal_portofolio_project/settings.py
+++ personal_portofolio_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1d98-i-xepuydx2s53yomp!15$lc00nxep@fyo%d*$ks!=y$ru'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['maikaru13.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
